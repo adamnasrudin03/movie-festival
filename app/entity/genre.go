1ba02750c65f9617ad6b24200b437e25116a7934
Adam Nasrudin <adamnasrudin858@gmail.com>
2023-05-15 11:42:32 +0700

    unique field name

--- app/entity/genre.go
+++ app/entity/genre.go
@@ -3,6 +3,6 @@ package entity
 // Genre represents the model for an Genre
 type Genre struct {
 	ID   uint64 `gorm:"primaryKey" json:"id"`
-	Name string `gorm:"not null" json:"name" `
+	Name string `gorm:"not null;uniqueIndex" json:"name" `
 	GORMModel
 }
