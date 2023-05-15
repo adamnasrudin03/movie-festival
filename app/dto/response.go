b0c5c5e4334cc523e9e74a00dff9fe776820ded5
Adam Nasrudin <adamnasrudin858@gmail.com>
2023-05-15 07:00:45 +0700

    dto response list

---
+++ app/dto/response.go
@@ -0,0 +1,16 @@
+package dto
+
+// ResponseList ...
+type ResponseList struct {
+	Total    uint64 `json:"total_data"`
+	Limit    uint64 `json:"limit"`
+	Page     uint64 `json:"page"`
+	LastPage uint64 `json:"last_page"`
+}
+
+type ListParam struct {
+	Page   uint64 `json:"page" validate:"required"`
+	Limit  uint64 `json:"limit" validate:"required"`
+	Offset uint64 `json:"offset"`
+	Search string `json:"search"`
+}
