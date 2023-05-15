107718bb857fd14374e4c7d0ff26e993904e21a2
Adam Nasrudin <adamnasrudin858@gmail.com>
2023-05-15 10:29:44 +0700

    adjust type duration

--- app/dto/movie.go
+++ app/dto/movie.go
@@ -3,7 +3,7 @@ package dto
 type CreateUpdateMovie struct {
 	Title       string `json:"title" validate:"required"`
 	Description string `json:"description" validate:"required"`
-	Duration    string `json:"duration" validate:"required"`
+	Duration    uint64 `json:"duration" validate:"required"`
 	WatchUrl    string `json:"watch_url" validate:"required"`
 	Artists     string `json:"artists" validate:"required"`
 	Genres      string `json:"genres" validate:"required"`
--- app/entity/movie.go
+++ app/entity/movie.go
@@ -5,7 +5,7 @@ type Movie struct {
 	ID          uint64 `gorm:"primaryKey" json:"id"`
 	Title       string `gorm:"not null" json:"title" `
 	Description string `gorm:"not null" json:"description"`
-	Duration    string `gorm:"not null" json:"duration"`
+	Duration    uint64 `gorm:"not null" json:"duration"`
 	WatchUrl    string `gorm:"not null" json:"watch_url"`
 	Artists     string `gorm:"not null" json:"artists"`
 	Genres      string `gorm:"not null" json:"genres"`
